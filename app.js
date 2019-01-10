b0ae8a842201874cdfe74993990d97a6f58f1500
Urbanek <marek.urbanek@accenture.com>
2019-01-10 11:44:58 +0100

    Added cors for frontend server

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const cors = require("cors")
 
-const usersRoutes = require("./users/users");
-const doctorsRoutes = require("./doctors/doctors");
-const commentsRoutes = require("./comments/comments");
+const startConnection = require("./db")
 
-const startConnection = require("./db");
+const usersRoutes = require("./users/users")
+const doctorsRoutes = require("./doctors/doctors")
+const commentsRoutes = require("./comments/comments")
+
+const corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
 
 app.use(bodyParser.urlencoded({
   extended: true
-}));
+}))
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+app.use(cors(corsOptions))
+app.use(express.static(__dirname + '/public'))
 
-app.use(express.static(__dirname + '/public'));
-
-app.use("/users", usersRoutes);
-app.use("/doctors", doctorsRoutes);
-app.use("/comments", commentsRoutes);
+app.use("/users", usersRoutes)
+app.use("/doctors", doctorsRoutes)
+app.use("/comments", commentsRoutes)
 
 app.listen(5000, function () {
-  console.log("Server is running..");
-  startConnection();
-});
+  console.log("Server is running..")
+  startConnection()
+})
