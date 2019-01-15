7722873dd3d91d94673c5a97683cfd2d13691001
Urbanek <marek.urbanek@accenture.com>
2019-01-15 14:46:40 +0100

    Refactored code using arrow functions

--- users/users.js
+++ users/users.js
@@ -1,47 +1,47 @@
 const express = require("express")
-const sql = require("mssql/msnodesqlv8");
-const bcrypt = require("bcrypt");
+const sql = require("mssql/msnodesqlv8")
+const bcrypt = require("bcrypt")
 
 const router = express.Router({
   mergeParams: true
 })
 
-router.post('/', function (req, res) {
-  const request = new sql.Request();
-  const query = `INSERT INTO users VALUES ('${req.body.username}', '${req.body.password}')`;
+router.post('/', (req, res) => {
+  const request = new sql.Request()
+  const query = `INSERT INTO users VALUES ('${req.body.username}', '${req.body.password}')`
   request.query(query)
     .then(result => {
-      res.send("new user created");
+      res.send("new user created")
       console.log(result)
     })
     .catch(err => {
-      console.log(err);
+      console.log(err)
     })
 })
 
-router.post('/register', function (req, res) {
-  const request = new sql.Request();
+router.post('/register', (req, res) => {
+  const request = new sql.Request()
 
-  bcrypt.hash(req.body.password, 10, function (err, hash) {
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
     if (!err) {
-      const query = `INSERT INTO users VALUES ('${req.body.username}', '${hash}')`;
+      const query = `INSERT INTO users VALUES ('${req.body.username}', '${hash}')`
 
       request.query(query)
         .then(result => {
-          res.send("new user created");
+          res.send("new user created")
           console.log(result)
         })
         .catch(err => {
-          console.log(err);
+          console.log(err)
         })
     } else {
-      console.log(err);
+      console.log(err)
     }
   })
 })
 
-router.post('/login', function (req, res) {
-  const request = new sql.Request();
+router.post('/login', (req, res) => {
+  const request = new sql.Request()
   getUserPasswordQuery = `SELECT password FROM users WHERE username = '${req.body.username}'`
   request.query(getUserPasswordQuery)
     .then(user => {
@@ -64,4 +64,4 @@ router.post('/login', function (req, res) {
     })
 })
 
-module.exports = router;
+module.exports = router
