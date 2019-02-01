44b25084a7bc41e8096aaf326e2aca64ea5b91a2
Urbanek <marek.urbanek@accenture.com>
2019-02-01 14:04:51 +0100

    Last comment feature

--- comments/comments.js
+++ comments/comments.js
@@ -6,15 +6,35 @@ const router = express.Router({
   mergeParams: true
 })
 
+router.get('/latest', (req, res) => {
+  const request = new sql.Request()
+
+  request.query(`
+      SELECT a.doctorId, a.text
+      FROM comments a
+      LEFT OUTER JOIN comments b
+      ON a.doctorId = b.doctorId 
+      AND a.created < b.created
+      WHERE b.doctorId IS NULL
+      ORDER BY a.created desc
+    `)
+    .then(comments => {
+      res.json(comments.recordset)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
 router.get('/:id', (req, res) => {
   const request = new sql.Request()
   const doctorId = req.params.id
 
   request.query(`
-    SELECT c.*, u.username FROM comments as c 
-    INNER JOIN users as u on c.userId = u.id
-    WHERE doctorId = ${doctorId} 
-    ORDER BY c.created desc
+      SELECT c.*, u.username FROM comments as c 
+      INNER JOIN users as u on c.userId = u.id
+      WHERE doctorId = ${doctorId} 
+      ORDER BY c.created desc
     `)
     .then(comments => {
       res.json(comments.recordset)
